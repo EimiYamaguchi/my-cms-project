45d35b78e34fb674fe8cfa2446ed650fd2f620bb
EimiYamaguchi <eimi.yamaguchi.1995@gmail.com>
2024-08-27 22:17:04 +0900

    feat: add notFound page

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import styles from "./page.module.css";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: {
@@ -9,7 +10,7 @@ type Props = {
   };
 };
 export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug);
+  const data = await getNewsDetail(params.slug).catch(notFound);
   return (
     <>
       <Article data={data} />
