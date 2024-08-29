a4196000114542cd0f74a5bcfa921ed5b9bfabfe
EimiYamaguchi <eimi.yamaguchi.1995@gmail.com>
2024-08-29 22:44:22 +0900

    feat: add category page

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,15 @@
+import NewsList from "@/app/_components/NewsList";
+import { getNewsList } from "@/app/_libs/microcms";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+export default async function Page({ params }: Props) {
+  const { contents: news } = await getNewsList({
+    filters: `category[equals]${params.id}`,
+  });
+
+  return <NewsList news={news} />;
+}
