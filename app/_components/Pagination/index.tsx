d2b6ed416962c813749e0c3f952bae9a306aaf54
EimiYamaguchi <eimi.yamaguchi.1995@gmail.com>
2024-08-31 21:08:08 +0900

    add pagination component

---
+++ app/_components/Pagination/index.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import styles from "./index.module.css";
+type Props = {
+  totalCount: number;
+  current: number;
+};
+
+export default async function Pagination({ totalCount, current = 1 }: Props) {
+  const pages = Array.from(
+    { length: Math.ceil(totalCount / 10) },
+    (_, i) => i + 1
+  );
+  return (
+    <nav>
+      <ul className={styles.container}>
+        {pages.map((page) => (
+          <li key={page} className={styles.list}>
+            {current !== page ? (
+              <Link href={`/news/p/${page}`} className={styles.item}>
+                {page}
+              </Link>
+            ) : (
+              <span className={`${styles.item} ${styles.current}`}>{page}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
